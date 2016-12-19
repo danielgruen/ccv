b7857ec72d6d60066ff53d1d3653d43517fa2593
Daniel Gruen <dgruen@stanford.edu>
2016-12-19 11:03:18 +0100

    updating corrh range of angular scales; need to recompute templates

--- src/corrh/template_corrh.h
+++ src/corrh/template_corrh.h
@@ -1,10 +1,10 @@
 // template_corrh.h: some settings for calculation of corrh templates
-// spanning angular range 0.1arcmin ... 60arcmin
+// spanning angular range 0.04arcmin ... 180arcmin
 
-const int rsteps=645;      // logarithmic steps in radius
+const int rsteps=846;      // logarithmic steps in radius
 const double rfactor=1.01; // 1% width annuli
 const double lnrfactor=log(rfactor);
-const double thetamin=0.1;  // arcmin
+const double thetamin=0.04;  // arcmin
 const double log10step=log10(rfactor);
 const double thetamax=thetamin*pow(rfactor,rsteps);
 
