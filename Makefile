cbc147ba8368137512f4aac4b44a81f61ef09da3
Daniel Gruen <dgruen@stanford.edu>
2017-03-30 21:04:58 -0700

    resampling test

--- Makefile
+++ Makefile
@@ -16,14 +16,14 @@
 
 ###########################################
 ### edit these to have the right c++ compiler and include/library paths for tmv, blas, CCfits
-CPP=g++
+CPP=g++ -fopenmp
 INCLUDES=-I ~/werc3/include -I ~/include 
 LIBFLAGS=-L /sw/lib -L ~/werc3/lib -L ~/lib -lCCfits -lcfitsio
 LIBFLAGS_TMV=-ltmv -ltmv_symband -lblas -lpthread
 LIBFLAGS_GSL=`gsl-config --libs`
 
 ### this is how many processors you'd like to use; only worry about this for non-pre-computed redshifts
-CORES=8
+CORES=1
 
 ### cluster definition file
 ### simple format with one line per cluster with ID z_lens p_z_source annuli_prefix
@@ -74,6 +74,8 @@ template_software: src/template_corrh src/template_corrh_combine src/template_co
 model_software: src/resample_ell src/resample_ell_g src/resample_off src/resample_off_g src/resample_conc src/resample_conc_g src/resample_conc_g src/resample_corrh src/resample_corrh_g src/getmodel src/getmodel_g src/getmodel_ds
 
 lut_software: src/calc_W 
+
+template_kappa_gamma_test: src/template_conc_g src/resample_conc_g_test templates_conc_g model_conc_g model_conc
 
 tinker: src/mktinkerconf
 	$(MAKE) -C src/tinker
@@ -137,6 +139,12 @@ templates_conc:
 	bash helpers/templates_conc.sh $(CORES)
 	@echo "========== finished with conc templates =========="
 
+templates_conc_g:
+	@echo "========== checking for availability of conc_g test templates =========="
+	bash helpers/templates_conc_g.sh $(CORES)
+	@echo "========== finished with conc_g test templates =========="
+
+
 templates_ell:
 	@echo "========== checking for availability of ell templates =========="
 	bash helpers/templates_ell.sh $(CORES)
@@ -163,6 +171,9 @@ src/template_corrh_combine: src/template_corrh_combine.cpp src/corrh/template_co
 src/template_conc: src/template_conc.cpp src/conc/template_conc.h src/enfw/enfw.h src/cosmology.h
 	$(CPP)  src/template_conc.cpp -o src/template_conc $(INCLUDES) $(LIBFLAGS) 
  
+src/template_conc_g: src/template_conc_g.cpp src/conc/template_conc.h src/enfw/enfw.h src/cosmology.h
+	$(CPP)  src/template_conc_g.cpp -o src/template_conc_g $(INCLUDES) $(LIBFLAGS) 
+ 
 src/template_ell: src/template_ell.cpp src/enfw/template_ell.h src/enfw/enfw.h src/cosmology.h src/filter/filter.o
 	$(CPP)  src/template_ell.cpp src/filter/filter.o -o src/template_ell $(INCLUDES) $(LIBFLAGS) 
  
@@ -185,6 +196,13 @@ model_conc:
 	$(eval ARGS=$(shell cat $(CLUSTERS) | cut -d \  -f 2,4 )) # a list of redshifts and annuli
 	bash helpers/model_conc.sh $(ARGS) $(CORES)
 	@echo "========== finished with conc model =========="
+
+model_conc_g:
+	@echo "========== checking for availability of conc_g test model =========="
+	$(eval ARGS=$(shell cat $(CLUSTERS) | cut -d \  -f 2,4 )) # a list of redshifts and annuli
+	bash helpers/model_conc_g.sh $(ARGS) $(CORES)
+	@echo "========== finished with conc model =========="
+
 
 model_ell:
 	@echo "========== checking for availability of ell model =========="
@@ -221,8 +239,8 @@ src/resample_conc: src/resample_conc.cpp src/conc/template_conc.h src/cosmology.
 src/resample_conc_g: src/resample_conc_g.cpp src/conc/template_conc.h src/cosmology.h
 	$(CPP)  src/resample_conc_g.cpp -o src/resample_conc_g $(INCLUDES) $(LIBFLAGS) $(LIBFLAGS_TMV)
 
-src/resample_conc_gamma: src/resample_conc_gamma.cpp src/conc/template_conc.h src/cosmology.h
-	$(CPP)  src/resample_conc_gamma.cpp -o src/resample_conc_gamma $(INCLUDES) $(LIBFLAGS) $(LIBFLAGS_TMV)
+src/resample_conc_g_test: src/resample_conc_g_test.cpp src/conc/template_conc.h src/cosmology.h
+	$(CPP)  src/resample_conc_g_test.cpp -o src/resample_conc_g_test $(INCLUDES) $(LIBFLAGS) $(LIBFLAGS_TMV)
 
 src/resample_corrh: src/resample_corrh.cpp src/corrh/template_corrh.h src/cosmology.h
 	$(CPP)  src/resample_corrh.cpp -o src/resample_corrh $(INCLUDES) $(LIBFLAGS) $(LIBFLAGS_TMV)
---
+++ src/resample_conc_g_test.cpp
@@ -0,0 +1,154 @@
+// compress an covariance matrix (conc) to the system of annuli used
+
+#include <assert.h>
+#include "cosmology.h"
+#include "conc/template_conc.h"
+#include <TMV.h>
+#include <iostream>
+
+using namespace std;
+using namespace tmv;
+
+#include <CCfits/CCfits>
+
+using namespace CCfits;
+
+
+int main(int argc, char **argv)
+{
+
+  if(argc!=5)
+  {
+   cout << "syntax: " << argv[0] << " [zlens] [annulus definition file] [logM*100] [destination]" << endl;
+   return 0;
+  }
+
+  double zlens=atof(argv[1]);
+  assert (zlens>0.);
+  double DLen = angularDiameterDistance(0,zlens,10000)*h; // hinv Mpc per rad
+
+  // (1) create annulus structure
+
+  ifstream ann(argv[2]);
+  int Nb; ann >> Nb;
+
+  double rmin[Nb];
+  double rmax[Nb];
+  double rmin2[Nb];
+  double rmax2[Nb];
+
+  for(int i=0; i<Nb; i++)
+  {
+    ann >> rmin[i] >> rmax[i];
+    rmin2[i]=rmin[i]*rmin[i];
+    rmax2[i]=rmax[i]*rmax[i];
+    assert(ann.eof()==0);
+  }
+  string buf;
+  ann >> buf;
+  assert(ann.eof());
+
+  // (3) create conversion matrix from one set of annuli to the other
+  
+  double m200m=pow10(atof(argv[3])/100.);
+  double c200m=halomodel::cmz_200m_duffy(m200m,zlens); // assume Duffy here, need to scale covariance to angles correctly
+  double r200m=halomodel::r200mRadius(m200m/h, zlens)*h/DLen*arcminperrad; // arcmin
+  
+  int    nc = int(c200m*10.+0.5);
+  assert(nc>=20);
+  assert(nc<=100);
+  
+  double rhatmin[nv];
+  double rhatmax[nv];
+  rhatmin[0]=pow10(vmin)*r200m;
+  rhatmax[0]=rhatmin[0]*vfac;
+  for(int j=1; j<nv; j++)
+  {
+    rhatmin[j]=rhatmax[j-1];
+    rhatmax[j]=rhatmin[j]*vfac;
+  }
+  
+  Matrix<double> A(Nb,nv);
+
+  cout << "# input covariance matrix: " << nv << " bins from " << rhatmin[0] << " to " << rhatmax[nv-1] << " arcmin" << endl;
+  cout << "# output covariance matrix: " << Nb << " bins from " << rmin[0] << " to " << rmax[Nb-1] << " arcmin" << endl;
+  
+  // kappa = A kappahat
+  for(int i=0; i<Nb; i++)
+  {
+    double areai = (rmax2[i]-rmin2[i]); // factor of PI consistently left out
+    for(int j=0; j<nv; j++)
+    {
+      if(rmax[i]<=rhatmin[j] || rmin[i]>=rhatmax[j]) // no overlap
+      {
+      A(i,j) = 0.;
+      continue;
+      }
+      double roverlapmin=max(rmin[i],rhatmin[j]);
+      double roverlapmax=min(rmax[i],rhatmax[j]);
+      A(i,j) = (pow(roverlapmax,2)-pow(roverlapmin,2))/areai;  // factor of PI consistently left out
+    }
+  }
+    
+  // (4) read in some covariance matrix
+  
+  Matrix<double> covhat(nv,nv);
+  
+  {
+    std::ostringstream ss;
+    ss << nc;
+    cout << "opening templates/conc_g/cov_"+ss.str()+".fits" << endl;
+    auto_ptr<FITS> pInfile(new FITS("templates/conc_g/cov_"+ss.str()+".fits",Read,true));
+    PHDU& image = pInfile->pHDU(); 
+    valarray<double>  contents;      
+    image.read(contents);
+    long ax1(image.axis(0));
+    long ax2(image.axis(1));
+  
+    cout << ax1 << " " << nv << endl;  
+    assert(ax1==nv);
+    assert(ax2==nv);
+    
+    int idx=0;
+    for(int i=0; i<nv; i++)
+      for(int j=0; j<nv; j++)
+      {
+	covhat(i,j)=contents[idx];
+	idx++;
+      }
+      
+  }
+  
+  Matrix<double> cov(Nb,Nb);
+  
+  cov = A*covhat*A.transpose();
+
+  {
+    FITS *pFits = 0; 
+    long naxis    =   2;      
+    long naxes[2] = { Nb, Nb };
+    try
+    {
+      const std::string fileName(argv[4]);            
+      pFits = new FITS(fileName, DOUBLE_IMG , naxis , naxes );
+    }
+    catch (FITS::CantCreate)
+    {
+      cerr << "error, can't create fits file" << endl;
+      return 1;
+    } 
+
+    valarray <float> image(0., Nb*Nb); // we're just going to jam our data in here ...
+    int idx=0;
+    for(int i=0; i<Nb; i++)
+      for(int j=0; j<Nb; j++)
+      {
+      image[idx]=cov(i,j);
+      idx++;
+      }
+    
+    pFits->pHDU().write(1,Nb*Nb,image);
+  }
+  
+  return 0;
+}
